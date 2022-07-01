a90c8d2a35737c29e300f65b0d35700c7fa0d396
navaneethamarnath <navaneethamarnath@gmail.com>
2022-07-01 14:09:30 +0400

    lasssssssst

--- function.js
+++ function.js
@@ -5,18 +5,23 @@ function sleep(ms) {
 window.onload=function(){
     document.getElementById('img').style.opacity='1';
     document.getElementById('img').style.transform='translate(0,-100);';
+    document.getElementById('img2').style.opacity='1';
+    document.getElementById('img2').style.transform='translate(0,-100);';
     console.log(Math.floor(Math.random() * 3))
     titlechange();
     bodyappear();   
 }
 function titlechange (){
     document.getElementById("qoute").hidden=false;
+    document.getElementById("qoute2").hidden=false;
     const qoutearr=["Welcome!","Hola!","Namaste!"]
     var qn=Math.floor(Math.random() * 3)
     document.getElementById("qoute").innerHTML=qoutearr[qn];
+    document.getElementById("qoute2").innerHTML=qoutearr[qn];
 }
 
 async function bodyappear(){
     await sleep(500);
     document.getElementById('links').hidden=false;
+    document.getElementById('links2').hidden=false;
 }
